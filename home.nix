c755a485009640bdd3e431ea69315b65cb4bb969
Eric Yen <eric@ericyen.com>
2023-08-21 06:30:05 +0000

    remap the leader key in vim tp spacebar,

--- home.nix
+++ home.nix
@@ -192,17 +192,46 @@ in
     defaultEditor = true;
     coc = { enable = true; };
     extraConfig = ''
+      " set the maplearder to the spacebar
+      " vim.g.mapleader = "<Space>"
+      let mapleader = " "
+
       set number relativenumber
       nnoremap <C-t> :NERDTreeToggle<CR>
       nnoremap <C-p> :FZF<CR>
+      " disbles mouse in neovim in general
       set mouse=
+
+      " for nerd commentary
+      filetype plugin on
+
+      " This is config for coc to enable tab completion and using enter to select
+      " Use tab for trigger completion with characters ahead and navigate
+      " NOTE: There's always complete item selected by default, you may want to enable
+      " no select by `"suggest.noselect": true` in your configuration file
+      " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+      " other plugin before putting this into your config
+      inoremap <silent><expr> <TAB>
+            \ coc#pum#visible() ? coc#pum#next(1) :
+            \ CheckBackspace() ? "\<Tab>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+      " Make <CR> to accept selected completion item or notify coc.nvim to format
+      " <C-g>u breaks current undo, please make your own choice
+      inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                                    \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
     '';
 
     plugins = with pkgs.vimPlugins; [
       vim-surround
       vim-gitgutter
       nerdtree
+      nerdtree-git-plugin
       fzfWrapper
+      #vim-commentary
+      vim-airline
+      nerdcommenter
     ];
   };
   #  home.file.".config/fish/config.fish".text = ''
